eb9fb81702ac3e88c93f111383f6b732b84b2089
eden-gelan <120310106+eden-gelan@users.noreply.github.com>
2023-06-03 00:09:12 +0300

    Add files via upload

---
+++ frontend/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\hp\\Documents\\FlutterDev\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\hp\\Desktop\\bloc_test" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\hp\\Documents\\FlutterDev\\flutter"
+  "PROJECT_DIR=C:\\Users\\hp\\Desktop\\bloc_test"
+  "FLUTTER_ROOT=C:\\Users\\hp\\Documents\\FlutterDev\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\hp\\Desktop\\bloc_test\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\hp\\Desktop\\bloc_test"
+  "FLUTTER_TARGET=C:\\Users\\hp\\Desktop\\bloc_test\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC85MGZhM2FlMjhmZTZkZGFlZTFhZjJjMTIwZjAxZTUwMjAxYzE0MDFiLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\hp\\Desktop\\bloc_test\\.dart_tool\\package_config.json"
+)
